59fb8818ac5a0bba2fddccd4913f5c123b5dd4b2
charukiesz <charulatapatel2002@yahoo.co.uk>
2022-05-22 21:43:30 +0100

    feat: add  refactor convertToRomanNumeral method to include tail recursion in RomanNumeralsConverter.java

--- src/main/java/RomanNumeralsConverter.java
+++ src/main/java/RomanNumeralsConverter.java
@@ -7,15 +7,18 @@ public class RomanNumeralsConverter {
     private static HashMap<Integer, String> numerals = new HashMap<>(){
         {
             put(1, "I");
-            put(2, "II");
-            put(3, "III");
+           // put(2, "II");
+            //put(3, "III");
             put(4, "IV");
         }
     };
 
     public String convertToRomanNumeral(int inputNumber) {
 
-         return numerals.get(inputNumber);
+        if (numerals.containsKey((inputNumber))){
+            return numerals.get(inputNumber);
+        }
+         return numerals.get(1) + convertToRomanNumeral(inputNumber -1) ;
     }
 
 
