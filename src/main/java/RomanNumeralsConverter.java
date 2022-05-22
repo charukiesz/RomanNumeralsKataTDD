a3db0f591077e854d9e8746445eb2fa77a7076b1
charukiesz <charulatapatel2002@yahoo.co.uk>
2022-05-22 21:33:50 +0100

    feat: add  refactor from String array to hashmap in RomanNumeralsConverter.java

--- src/main/java/RomanNumeralsConverter.java
+++ src/main/java/RomanNumeralsConverter.java
@@ -1,14 +1,21 @@
+import java.util.HashMap;
 public class RomanNumeralsConverter {
 
 
-    RomanNumeralsConverter() {
-    }
+    RomanNumeralsConverter() {}
 
-    private String [] numerals = {"I", "II", "III", "IV"};
+    private static HashMap<Integer, String> numerals = new HashMap<>(){
+        {
+            put(1, "I");
+            put(2, "II");
+            put(3, "III");
+            put(4, "IV");
+        }
+    };
 
     public String convertToRomanNumeral(int inputNumber) {
 
-         return numerals[inputNumber -1];
+         return numerals.get(inputNumber);
     }
 
 
